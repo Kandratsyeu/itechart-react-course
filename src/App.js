ee7db0a040c193cf9889d6463087c566cff0ef96
Siarhei Kandratsyeu <sukandratsyeu@gmail.com>
2021-08-30 21:08:20 +0300

    feat: base component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
 import './App.css'
+import Base from './components/base'
 
 function App() {
   return (
     <div className="App">
-      <h1>Itechart React course</h1>
-      <section className="cards">
-        <div className="card">
-          <h2>First card</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
-            explicabo aperiam repudiandae dolor voluptates dolorem earum tempora
-            vero veniam, illum dicta molestiae nisi laborum molestias expedita
-            iusto, odit odio. Odio?
-          </p>
-        </div>
-        <div className="card">
-          <h2>Second card</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
-            explicabo aperiam repudiandae dolor voluptates dolorem earum tempora
-            vero veniam, illum dicta molestiae nisi laborum molestias expedita
-            iusto, odit odio. Odio?
-          </p>
-        </div>
-        <div className="card">
-          <h2>Third card</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
-            explicabo aperiam repudiandae dolor voluptates dolorem earum tempora
-            vero veniam, illum dicta molestiae nisi laborum molestias expedita
-            iusto, odit odio. Odio?
-          </p>
-        </div>
-      </section>
+      <Base></Base>
     </div>
   )
 }
---
+++ src/components/base.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Base() {
+  return (
+    <>
+      <h1>Itechart React course</h1>
+      <section className="cards">
+        <div className="card">
+          <h2>First card</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
+            explicabo aperiam repudiandae dolor voluptates dolorem earum tempora
+            vero veniam, illum dicta molestiae nisi laborum molestias expedita
+            iusto, odit odio. Odio?
+          </p>
+        </div>
+        <div className="card">
+          <h2>Second card</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
+            explicabo aperiam repudiandae dolor voluptates dolorem earum tempora
+            vero veniam, illum dicta molestiae nisi laborum molestias expedita
+            iusto, odit odio. Odio?
+          </p>
+        </div>
+        <div className="card">
+          <h2>Third card</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
+            explicabo aperiam repudiandae dolor voluptates dolorem earum tempora
+            vero veniam, illum dicta molestiae nisi laborum molestias expedita
+            iusto, odit odio. Odio?
+          </p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Base
